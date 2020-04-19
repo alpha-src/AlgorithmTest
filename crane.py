b3e0f2526c1e8d0b15b0bd21cc46dbd2c84c29ae
Sihyun Jung <54366260+alpha-src@users.noreply.github.com>
2020-04-20 00:02:37 +0900

    Update crane.py

--- crane.py
+++ crane.py
@@ -25,11 +25,5 @@ def solution(board, moves):
 
 
     return answer*2
-board = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
-moves = [1,5,3,5,1,2,1,4]
-
-result = solution(board, moves)
-
-print(result)
 
 
