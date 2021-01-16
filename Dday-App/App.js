de96045f575db9504b26e586346afa02068ff69a
Sihyun Jung <54366260+alpha-src@users.noreply.github.com>
2021-01-16 04:19:05 +0000

    Make Basic Structure of Application

--- Dday-App/App.js
+++ Dday-App/App.js
@@ -1,14 +1,6 @@
-import 'react-native-gesture-handler';
-import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import TabNavigator from './src/Components/TabNavigator';
+import * as React from 'react';
+import ScreenContainer from './src/Components';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <NavigationContainer>
-        <TabNavigator />
-      </NavigationContainer>
-    );
-  }
+export default function App() {
+  return <ScreenContainer />;
 }
